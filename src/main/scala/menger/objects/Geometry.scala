747f6041114835555565842adf86eaab5c529f12
Lene <lene.preuss@exxeta.com>
2025-09-17 13:44:51 +0200

    WIP: overlay of multiple geometries

--- src/main/scala/menger/objects/Geometry.scala
+++ src/main/scala/menger/objects/Geometry.scala
@@ -13,6 +13,7 @@ trait Geometry(
   def at(center: Vector3, scale: Float = 1): List[ModelInstance]
   override def toString: String = getClass.getSimpleName
   override def handleEvent(event: RotationProjectionParameters): Unit = {}
+  
   def logTime[T](msg: String, minDuration: Int = 0)(f: => T): T =
     val start = System.currentTimeMillis()
     val result = f
@@ -20,3 +21,7 @@ trait Geometry(
     if duration >= minDuration && Gdx.app != null then
     Gdx.app.log(s"${getClass.getSimpleName}.$msg", s"${duration}ms")
     result
+
+  def composite(other: Geometry): Geometry =
+    ???
+    
