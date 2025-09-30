lene/menger

commit 5a836c544a0da1455bb1f21df045009d7785158b

RotationProjectionParameters from the command line options for the 4D rotations

src/main/scala/menger/InteractiveMengerEngine.scala
@@ -1,0 +1,47 @@
+package menger
+
+import com.badlogic.gdx.graphics.g3d.{Material, ModelInstance}
+import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.utils.Timer
+import com.badlogic.gdx.Gdx
+import com.typesafe.scalalogging.LazyLogging
+import menger.input.EventDispatcher
+import menger.objects.Geometry
+
+
+class InteractiveMengerEngine(
+  spongeType: String = "square", spongeLevel: Int = 0,
+  rotationProjectionParameters: RotationProjectionParameters = RotationProjectionParameters(),
+  lines: Boolean = false, timeout: Float = 0
+) extends MengerEngine(spongeType, spongeLevel, rotationProjectionParameters, lines)
+with LazyLogging:
+
+  private lazy val sponge: Geometry = generateObject(spongeType, spongeLevel, material, primitiveType)
+  private lazy val eventDispatcher = EventDispatcher()
+  "tesseract".r.findFirstIn(spongeType).foreach(_ => eventDispatcher.addObserver(sponge))
+
+  protected def drawables: List[ModelInstance] = sponge.at(Vector3(0, 0, 0), 1)
+  protected def gdxResources: GDXResources = GDXResources(Some(eventDispatcher))
+
+  override def create(): Unit =
+    Gdx.app.log(s"${getClass.getSimpleName}.create()", sponge.toString())
+    if timeout > 0 then startExitTimer(timeout)
+
+  override def render(): Unit = gdxResources.render(drawables)
+
+  override def pause(): Unit =
+    super.pause()
+    Timer.instance().stop()
+
+  override def resume(): Unit =
+    super.resume()
+    Timer.instance().start()
+
+  override def dispose(): Unit =
+    super.dispose()
+    Timer.instance().stop()
+
+  private def startExitTimer(timeout: Float): Unit =
+    logger.info(s"Starting timer for $timeout seconds")
+    Timer.schedule(() => Gdx.app.exit(), timeout, 0)
+

src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 
 import com.badlogic.gdx.backends.lwjgl3.{Lwjgl3Application, Lwjgl3ApplicationConfiguration}
-import menger.{MengerCLIOptions, MengerEngine, RotationProjectionParameters}
+import menger.{AnimatedMengerEngine, InteractiveMengerEngine, MengerCLIOptions, MengerEngine, RotationProjectionParameters}
 
 object Main:
   private final val COLOR_BITS = 8
@@ -10,9 +10,7 @@ object Main:
   def main(args: Array[String]): Unit =
     val opts = MengerCLIOptions(args.toList)
     val config = getConfig(opts)
-    val rotationProjectionParameters = RotationProjectionParameters(opts)
-    
-    val rendering = MengerEngine(opts.timeout(), opts.level(), opts.lines(), opts.spongeType(), rotationProjectionParameters)
+    val rendering = createEngine(opts)
     Lwjgl3Application(rendering, config)
 
   def getConfig(opts: MengerCLIOptions): Lwjgl3ApplicationConfiguration =
@@ -25,3 +23,15 @@ object Main:
       opts.antialiasSamples()
     )
     config
+
+  def createEngine(opts: MengerCLIOptions): MengerEngine =
+    val rotationProjectionParameters = RotationProjectionParameters(opts)
+    if opts.animate().parts.nonEmpty then
+      AnimatedMengerEngine(
+        opts.spongeType(), opts.level(), rotationProjectionParameters, opts.lines(), opts.animate()
+      )
+    else
+      InteractiveMengerEngine(
+        opts.spongeType(), opts.level(), rotationProjectionParameters, opts.lines(), opts.timeout()
+      )
+

src/main/scala/menger/AnimatedMengerEngine.scala
@@ -1,0 +1,41 @@
+package menger
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g3d.ModelInstance
+import com.badlogic.gdx.math.Vector3
+
+class AnimatedMengerEngine(
+  spongeType: String, spongeLevel: Int,
+  rotationProjectionParameters: RotationProjectionParameters = RotationProjectionParameters(),
+  lines: Boolean, val animationSpecifications: AnimationSpecifications
+) extends MengerEngine(spongeType, spongeLevel, rotationProjectionParameters, lines):
+  private var currentFrame: Int = 0
+
+  protected def drawables: List[ModelInstance] = 
+    generateObject(spongeType, spongeLevel, material, primitiveType).at(Vector3(0, 0, 0), 1)
+
+  protected def gdxResources: GDXResources = GDXResources(None)
+
+  override def currentRotProj: RotationProjectionParameters =
+    rotationProjectionParameters + animationSpecifications.rotationProjectionParameters(currentFrame)
+  override def create(): Unit =
+    Gdx.app.log(
+      s"${getClass.getSimpleName}.create()",
+      s"Animating for $animationSpecifications"
+    )
+
+  override def render(): Unit =
+    super.render()
+    gdxResources.render(drawables)
+    nextStep()
+    
+
+  private def nextStep(): Unit = 
+    currentFrame += 1
+    if currentFrame >= animationSpecifications.numFrames then
+      Gdx.app.exit()
+    Gdx.app.log(
+      s"${getClass.getSimpleName}.nextStep()",
+      s"Current frame: $currentFrame"
+    )
+    

src/main/scala/menger/AnimationSpecification.scala
@@ -3,7 +3,7 @@ package menger
 import scala.util.Try
 import com.typesafe.scalalogging.LazyLogging
 
-class AnimationSpecification(s: String) extends LazyLogging:
+case class AnimationSpecification(s: String) extends LazyLogging:
 
   type StartEnd = (Float, Float)
 
@@ -20,12 +20,47 @@ class AnimationSpecification(s: String) extends LazyLogging:
 
   def valid(spongeType: String): Boolean = timeSpecValid && animationParametersValid(spongeType)
 
+  override def toString: String =
+    val timeSpec = (seconds, frames) match
+      case (Some(s), None) => s"seconds=$s"
+      case (None, Some(f)) => s"frames=$f"
+      case _ => throw IllegalArgumentException("Invalid animation specification")
+    val animationSpec = animationParameters.mkString(":")
+    s"$timeSpec:$animationSpec"
+
+  def rotationProjectionParameters(frame: Int): RotationProjectionParameters =
+    def current(bounds: (Float, Float)): Float = {
+      bounds._1 + (bounds._2 - bounds._1) * frame / frames.get
+    }
+
+    require(animationParameters.nonEmpty, "AnimationSpecification.animationParameters not defined")
+    val rotXBounds: (Float, Float) = animationParameters.get("rot-x").getOrElse(0f, 0f)
+    val rotYBounds: (Float, Float) = animationParameters.get("rot-y").getOrElse(0f, 0f)
+    val rotZBounds: (Float, Float) = animationParameters.get("rot-z").getOrElse(0f, 0f)
+    val rotXWBounds: (Float, Float) = animationParameters.get("rot-x-w").getOrElse(0f, 0f)
+    val rotYWBounds: (Float, Float) = animationParameters.get("rot-y-w").getOrElse(0f, 0f)
+    val rotZWBounds: (Float, Float) = animationParameters.get("rot-z-w").getOrElse(0f, 0f)
+    val screenWBounds: (Float, Float) = animationParameters.get("projection-screen-w")
+      .getOrElse(Const.defaultScreenW, Const.defaultScreenW)
+    val eyeWBounds: (Float, Float) = animationParameters.get("projection-eye-w")
+      .getOrElse(Const.defaultEyeW, Const.defaultEyeW)
+    RotationProjectionParameters(
+      rotX = current(rotXBounds),
+      rotY = current(rotYBounds),
+      rotZ = current(rotZBounds),
+      rotXW = current(rotXWBounds),
+      rotYW = current(rotYWBounds),
+      rotZW = current(rotZWBounds),
+      screenW = current(screenWBounds),
+      eyeW = current(eyeWBounds)
+    )
+
   private def parseStartEnd(s: String): StartEnd =
-    val splitted = s.split('-')
-    require(splitted.length == 2, s"Invalid start-end specification: $s")
-    require(splitted.head.toFloatOption.isDefined, s"Start ${splitted.head} not a Float")
-    require(splitted.last.toFloatOption.isDefined, s"End ${splitted.last} not a Float")
-    (splitted.head.toFloat, splitted.last.toFloat)
+    val parts = s.split('-')
+    require(parts.length == 2, s"Invalid start-end specification: $s")
+    require(parts.head.toFloatOption.isDefined, s"Start ${parts.head} not a Float")
+    require(parts.last.toFloatOption.isDefined, s"End ${parts.last} not a Float")
+    (parts.head.toFloat, parts.last.toFloat)
 
   private def timeSpecValid: Boolean =
     (seconds.nonEmpty && seconds.get > 0) ^ (frames.nonEmpty && frames.get > 0)
@@ -51,6 +86,35 @@ object AnimationSpecification:
     )
 
 
-case class AnimationSpecifications(specification: List[String] = List.empty):
+case class AnimationSpecifications(specification: List[String] = List.empty) extends LazyLogging:
   val parts: List[AnimationSpecification] = specification.map(AnimationSpecification(_))
-  def valid(spongeType: String): Boolean = parts.forall(_.valid(spongeType))
+  val numFrames: Int = parts.map(_.frames.getOrElse(0)).sum
+
+  def valid(spongeType: String): Boolean =
+    parts.forall(_.valid(spongeType)) && parts.map(_.seconds).map(_.isDefined).toSet.size < 2
+
+  def rotationProjectionParameters(frame: Int): RotationProjectionParameters =
+    partAndFrame(frame).map((specList, frame) => accumulateAllButLastRotationProjections(specList) + specList.last.rotationProjectionParameters(frame)).getOrElse(
+      throw IllegalArgumentException("AnimationSpecification.frames not defined")
+    )
+
+  def accumulateAllButLastRotationProjections(specs: List[AnimationSpecification]): RotationProjectionParameters =
+    specs.init.foldLeft(RotationProjectionParameters()) { (acc, spec) =>
+      acc + spec.rotationProjectionParameters(spec.frames.getOrElse(0))
+    }
+
+  def partAndFrame(
+    totalFrame: Int,
+    partsParts: List[AnimationSpecification] = parts,
+    accumulator: List[AnimationSpecification] = List.empty
+  ): Try[(List[AnimationSpecification], Int)] =
+    if partsParts.isEmpty then
+       throw IllegalArgumentException("AnimationSpecification.parts not defined")
+    else
+      val current = partsParts.head
+      if current.frames.isEmpty then
+        throw IllegalArgumentException(s"Animation specification $current has no frames")
+      else
+        if current.frames.getOrElse(0) > totalFrame then
+          Try((accumulator :+ current, totalFrame))
+        else partAndFrame(totalFrame - current.frames.getOrElse(0), partsParts.tail, accumulator :+ current)

src/main/scala/menger/Const.scala
@@ -1,0 +1,9 @@
+package menger
+
+case object Const:
+  val epsilon: Float = 1e-5
+  val defaultWindowWidth = 800
+  val defaultWindowHeight = 600
+  val defaultAntialiasSamples = 4
+  val defaultScreenW = 1f
+  val defaultEyeW = 2f

src/main/scala/menger/GDXResources.scala
@@ -13,12 +13,12 @@ import scala.collection.immutable.List
 import scala.jdk.CollectionConverters.*
 
 
-case class GDXResources(eventDispatcher: EventDispatcher):
+case class GDXResources(eventDispatcher: Option[EventDispatcher]):
 
   private val cameraPosition = Vector3(-2f, 1f, -1f)
   private val environment: Environment = createEnvironment
   private val camera: PerspectiveCamera = createCamera(cameraPosition)
-  Gdx.input.setInputProcessor(MengerInputMultiplexer(camera, eventDispatcher))
+  eventDispatcher.foreach(e => Gdx.input.setInputProcessor(MengerInputMultiplexer(camera, e)))
   private val modelBatch = ModelBatch()
   private val fpsLogger = FPSLogger()
 

src/main/scala/menger/MengerCLIOptions.scala
@@ -12,10 +12,10 @@ class MengerCLIOptions(arguments: Seq[String]) extends ScallopConf(arguments):
     required = false, default = Some("square")
   )
   val projectionScreenW: ScallopOption[Float] = opt[Float](
-    required = false, default = Some(1), validate = _ > 0
+    required = false, default = Some(Const.defaultScreenW), validate = _ > 0
   )
   val projectionEyeW: ScallopOption[Float] = opt[Float](
-    required = false, default = Some(2), validate = _ > 0
+    required = false, default = Some(Const.defaultEyeW), validate = _ > 0
   )
   private def degreeOpt = opt[Float](
       required = false, default = Some(0), validate = a => a >= 0 && a < 360
@@ -25,16 +25,23 @@ class MengerCLIOptions(arguments: Seq[String]) extends ScallopConf(arguments):
   val rotZW: ScallopOption[Float] = degreeOpt
   val level: ScallopOption[Int] = opt[Int](required = false, default = Some(1), validate = _ >= 0)
   val lines: ScallopOption[Boolean] = opt[Boolean](required = false, default = Some(false))
-  val width: ScallopOption[Int] = opt[Int](required = false, default = Some(800))
-  val height: ScallopOption[Int] = opt[Int](required = false, default = Some(600))
-  val antialiasSamples: ScallopOption[Int] = opt[Int](required = false, default = Some(4))
-  // TODO: use custom converters for AnimationSpecification, see https://github.com/scallop/scallop/wiki/Custom-converters
-  val animate: ScallopOption[AnimationSpecifications] = opt[AnimationSpecifications]()(animationSpecificationsConverter)
+  val width: ScallopOption[Int] = opt[Int](
+    required = false, default = Some(Const.defaultWindowWidth)
+  )
+  val height: ScallopOption[Int] = opt[Int](
+    required = false, default = Some(Const.defaultWindowHeight)
+  )
+  val antialiasSamples: ScallopOption[Int] = opt[Int](
+    required = false, default = Some(Const.defaultAntialiasSamples)
+  )
+  val animate: ScallopOption[AnimationSpecifications] = opt[AnimationSpecifications]()(
+    animationSpecificationsConverter
+  )
   validate(projectionScreenW, projectionEyeW) { (screen, eye) =>
     if eye > screen then Right(())
     else Left("eyeW must be greater than screenW")
   }
-  validate(animate, spongeType) { (start, sponge) => validateAnimationSpecification(start, sponge) }
+  validate(animate, spongeType) { (spec, sponge) => validateAnimationSpecification(spec, sponge) }
   verify()
 
   private def validateAnimationSpecification(spec: AnimationSpecifications, spongeType: String) =
@@ -45,13 +52,10 @@ class MengerCLIOptions(arguments: Seq[String]) extends ScallopConf(arguments):
     case ScallopException(message) => throw IllegalArgumentException(message)
     case other => throw other
 
-val animationSpecificationsConverter: ValueConverter[AnimationSpecifications] = new ValueConverter[AnimationSpecifications] {
+val animationSpecificationsConverter = new ValueConverter[AnimationSpecifications] {
   val argType = org.rogach.scallop.ArgType.LIST
   def parse(s: List[(String, List[String])]): Either[String, Option[AnimationSpecifications]] =
-    val specificationStrings = s.flatMap(_(1))
-    Try {
-      Right(Some(AnimationSpecifications(specificationStrings)))
-    }.recover {
-      case e: Exception => Left(e.getMessage)
-    }.get
+    val specStrings = s.flatMap(_(1))
+    Try { Right(Some(AnimationSpecifications(specStrings)))
+    }.recover { case e: Exception => Left(e.getMessage) }.get
 }

src/main/scala/menger/MengerEngine.scala
@@ -1,53 +1,36 @@
 package menger
 
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g3d.{Material, ModelInstance}
-import com.badlogic.gdx.math.Vector3
-import com.badlogic.gdx.utils.Timer
-import com.badlogic.gdx.{Game, Gdx}
-import menger.input.EventDispatcher
-import menger.objects.higher_d.{RotatedProjection, Tesseract, TesseractSponge, TesseractSponge2}
 import menger.objects.{Builder, Geometry, SpongeBySurface, SpongeByVolume}
+import menger.objects.higher_d.{RotatedProjection, Tesseract, TesseractSponge, TesseractSponge2}
 
-class MengerEngine(
-  timeout: Float = 0, spongeLevel: Int = 0, lines: Boolean = false, spongeType: String = "square",
-  rotationProjectionParameters: RotationProjectionParameters = RotationProjectionParameters()
+abstract class MengerEngine(
+  val spongeType: String, val spongeLevel: Int,
+  val rotationProjectionParameters: RotationProjectionParameters, val lines: Boolean
 ) extends Game:
-
-  private val material = Builder.WHITE_MATERIAL
-  private lazy val primitiveType = if lines then GL20.GL_LINES else GL20.GL_TRIANGLES
-  private lazy val sponge: Geometry = generateObject(spongeType, spongeLevel, material, primitiveType)
-  private lazy val eventDispatcher = EventDispatcher()
-  "tesseract".r.findFirstIn(spongeType).foreach(_ => eventDispatcher.addObserver(sponge))
-  private def drawables: List[ModelInstance] = sponge.at(Vector3(0, 0, 0), 1)
-  private lazy val gdxResources = GDXResources(eventDispatcher)
-
-  override def create(): Unit =
-    Gdx.app.log("create", sponge.toString())
-    if timeout > 0 then
-      Timer.schedule(() => Gdx.app.exit(), timeout, 0)
-
-  override def render(): Unit = gdxResources.render(drawables)
-
-  override def dispose(): Unit = gdxResources.dispose()
-
+  protected val material: Material = Builder.WHITE_MATERIAL
+  protected lazy val primitiveType: Int = if lines then GL20.GL_LINES else GL20.GL_TRIANGLES
+  protected def gdxResources: GDXResources
+  protected def drawables: List[ModelInstance]
   override def resume(): Unit = {}
-
-  override def resize(width: Int, height: Int): Unit = gdxResources.resize()
-
   override def pause(): Unit = {}
+  override def dispose(): Unit = gdxResources.dispose()
+  override def resize(width: Int, height: Int): Unit = gdxResources.resize()
+  def currentRotProj: RotationProjectionParameters = rotationProjectionParameters
 
-  private def generateObject(spongeType: String, level: Int, material: Material, primitiveType: Int): Geometry =
+  protected def generateObject(
+    spongeType: String, level: Int, material: Material, primitiveType: Int
+  ): Geometry =
     spongeType match
       case "square" => SpongeBySurface(level, material, primitiveType)
       case "cube" => SpongeByVolume(level, material, primitiveType)
-      case "tesseract" => RotatedProjection(
-        Tesseract(), rotationProjectionParameters, material, primitiveType
-      )
+      case "tesseract" => RotatedProjection(Tesseract(), currentRotProj, material, primitiveType)
       case "tesseract-sponge" => RotatedProjection(
-        TesseractSponge(level), rotationProjectionParameters, material, primitiveType
+        TesseractSponge(level), currentRotProj, material, primitiveType
       )
       case "tesseract-sponge-2" => RotatedProjection(
-        TesseractSponge2(level), rotationProjectionParameters, material, primitiveType
+        TesseractSponge2(level), currentRotProj, material, primitiveType
       )
       case _ => throw new IllegalArgumentException(s"Unknown sponge type: $spongeType")

commit 26410ca37df5e7861e36bd3909bdc69817ea4036

fix keyboard/mouse interaction issue introduced in 5a836c544

src/main/scala/menger/InteractiveMengerEngine.scala
@@ -22,14 +22,10 @@ with LazyLogging:
     case scala.util.Failure(exception) =>
       logger.error(s"Failed to create sponge type '$spongeType': ${exception.getMessage}")
       sys.exit(1)
-  private lazy val eventDispatcher = {
-    val dispatcher = EventDispatcher()
-    "tesseract".r.findFirstIn(spongeType).foreach(_ => dispatcher.addObserver(sponge))
-    dispatcher
-  }
+  private lazy val eventDispatcher = dispatcherWithRegisteredSponge
 
   protected def drawables: List[ModelInstance] = sponge.at()
-  protected def gdxResources: GDXResources = GDXResources(Some(eventDispatcher))
+  protected lazy val gdxResources: GDXResources = GDXResources(Some(eventDispatcher))
 
   override def create(): Unit =
     Gdx.app.log(s"${getClass.getSimpleName}.create()", s"$sponge color=$color")
@@ -53,3 +49,7 @@ with LazyLogging:
     logger.info(s"Starting timer for $timeout seconds")
     Timer.schedule(() => Gdx.app.exit(), timeout, 0)
 
+  private def dispatcherWithRegisteredSponge =
+    val dispatcher = EventDispatcher()
+    "tesseract".r.findFirstIn(spongeType).foreach(_ => dispatcher.addObserver(sponge))
+    dispatcher